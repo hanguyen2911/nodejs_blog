045f3aaffc0787c0cb0c62688e3e41d9fc14fae9
hanguyen2911 <nguyenthithuha2911ntth@gmail.com>
2023-05-05 17:10:59 +0700

    feat: create node & express

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars').engine;;
 const app = express();
 const port = 3000;
-
+//express.static: 1 middleware tích hợp trong ExpessJS phục hồi tài nguyên tĩnh từ thư mục public
+//__dirname biến toàn cục trong NodeJS
 app.use(express.static(path.join(__dirname, 'public')))
-
+//app.use: đăng kí middleware
 app.use(morgan('combined'));
 app.engine('hbs', handlebars({
   extname: '.hbs'
